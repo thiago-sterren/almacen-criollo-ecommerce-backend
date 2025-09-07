2190bb945ed7122d4314baa82171ea72d9308ef5
thiago-sterren <thiagosterren@gmail.com>
2025-09-07 11:35:37 -0300

    new changes in database.js

--- config/database.js
+++ config/database.js
@@ -27,7 +27,7 @@ module.exports = ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'AlmacenCriolloEcommerce'),
+        database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
