9215806e515e9ef5656c7648b465969fa8143291
thiago-sterren <thiagosterren@gmail.com>
2025-10-03 11:12:58 -0300

    fixing emails again

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -80,11 +80,10 @@ module.exports = {
       
       // 2. Si el método es efectivo -> devolver confirmación
       if (paymentMethod === "cash") {
+        const pickupText = `Ya podés pasar a retirar tu orden y realizar el pago de ${formatPrice(totalPrice)} por nuestro local en J. B. Justo 361. Horarios: 9 a 12 y 16:30 a 20 horas.`
+        const deliveryText = `Te contactaremos para acordar detalles de la entrega a realizar en ${address}. Total a pagar en el momento de entrega: ${formatPrice(totalPrice)}.`
         const emailHTML = `<strong>¡Gracias por elegirnos, ${firstName}!</strong>
-        <p>${deliveryMethod === "pickup" ?
-          `Ya podés pasar a retirar tu orden y realizar el pago de ${formatPrice(totalPrice)} por nuestro local en J. B. Justo 361. Horarios: 9 a 12 y 16:30 a 20 horas.` :
-          `Te contactaremos para acordar detalles de la entrega a realizar en ${address}. Total a pagar en el momento de entrega: ${formatPrice(totalPrice)}.`
-        }
+        <p>${deliveryMethod === "pickup" ? pickupText : deliveryText}
         <br><br>
         Por favor, no responder a esta dirección de email. Ante cualquier duda, contactanos a nuestro WhatsApp o Instagram que podés encontrar en el pie de la misma página web en la que hiciste esta compra :)
         </p>`
@@ -150,12 +149,13 @@ module.exports = {
 
         // Enviar email de "orden realizada con Mercado Pago"
         const orderLink = `${process.env.FRONTEND_URL}/order/${orderToken}`
+        const linkText = `Te notificaremos por esta vía con novedades sobre el estado tu pedido, aunque podés guardar este link para hacer el seguimiento: <a href=${orderLink}>${orderLink}</a>.`
         const emailHTML = `<strong>¡Gracias por elegirnos, ${firstName}!</strong>
         <p>Estamos esperando que Mercado Pago nos haga llegar la confirmación de tu pago, mientras tanto, <strong>te solicitamos que nos envíes tu comprobante de pago vía WhatsApp o Instagram.</strong>
         <br>
         <strong>Aclaración:</strong> si no recibimos la confirmación dentro de las 48hs posteriores a la creación de la orden, cancelaremos la misma.
         <br>
-        Te notificaremos por esta vía con novedades sobre el estado tu pedido, aunque podés guardar este link para hacer el seguimiento: <a href=${orderLink}>${orderLink}</a>.
+        ${linkText}
         <br><br>
         Por favor, no responder a esta dirección de email. Ante cualquier duda, contactanos a nuestro WhatsApp o Instagram que podés encontrar en el pie de la misma página web en la que hiciste esta compra :)
         </p>`
